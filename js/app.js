8059c8b2e351d7a18965e3300e759ab6c424d9bd
zamarwint <zamar.wint1@gmail.com>
2026-04-09 23:12:14 -0500

    Fixed the light / dark mode, the app now remembers the light / dark mode state, fixed the dialog x icon, added animations, added light / dark mode to the app part, expanded the width for generate pass button, styled the scrollbar, added back button, and many other bug fixes.

--- js/app.js
+++ js/app.js
@@ -33,6 +33,43 @@ closeErrorModal.addEventListener("click", () => {
 //   errorDialog.showModal();
 // });
 
+// Handling light / dark mode the right way
+const toggle = document.querySelector("#toggle");
+
+// Dark mode by default
+if (!localStorage.getItem("theme")) {
+  localStorage.setItem("theme", "dark");
+}
+
+if (localStorage.getItem("theme") === "light") {
+  // If it was stored as light, apply light mode.
+  document.body.classList.toggle("light-mode");
+  toggle.className = "fa-solid fa-moon";
+  toggle.style.color = "#fff";
+  closeSuccessModal.style.fill = "#000";
+  closeErrorModal.style.fill = "#000";
+}
+
+toggle.addEventListener("click", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    // Change it to light
+    localStorage.setItem("theme", "light");
+    document.body.classList.toggle("light-mode");
+    toggle.className = "fa-solid fa-moon";
+    toggle.style.color = "#fff";
+    closeSuccessModal.style.fill = "#000";
+    closeErrorModal.style.fill = "#000";
+  } else if (localStorage.getItem("theme") === "light") {
+    // Change it to dark
+    localStorage.setItem("theme", "dark");
+    document.body.classList.remove("light-mode");
+    toggle.className = "fa-solid fa-sun";
+    toggle.style.color = "#fff";
+    closeSuccessModal.style.fill = "#fff";
+    closeErrorModal.style.fill = "#fff";
+  }
+});
+
 // Password Generate Function
 function passwordGenerate() {
   // Get Password Input Field
--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
+// Handling light and dark mode the right way.
 const toggle = document.querySelector("#toggle");
 
+// Dark mode by default
+if (!localStorage.getItem("theme")) {
+  localStorage.setItem("theme", "dark");
+}
+
+if (localStorage.getItem("theme") === "light") {
+  // If it was stored as light, apply light mode.
+  document.body.classList.toggle("light-mode");
+  toggle.className = "fa-solid fa-moon";
+  toggle.style.color = "#000";
+}
+
 toggle.addEventListener("click", () => {
-  if (document.body.classList.contains("light-mode")) {
-    document.body.classList.toggle("light-mode");
-    toggle.className = "fa-solid fa-sun";
-    toggle.style.color = "#fff";
-  } else {
+  if (localStorage.getItem("theme") === "dark") {
+    // Change it to light
+    localStorage.setItem("theme", "light");
     document.body.classList.toggle("light-mode");
     toggle.className = "fa-solid fa-moon";
     toggle.style.color = "#000";
+  } else if (localStorage.getItem("theme") === "light") {
+    // Change it to dark
+    localStorage.setItem("theme", "dark");
+    document.body.classList.remove("light-mode");
+    toggle.className = "fa-solid fa-sun";
+    toggle.style.color = "#fff";
   }
 });
